e8d70feafe17e6ac52e3a28227caff62204650bf
Gregory <gaoadt@outlook.com>
2019-12-09 19:10:03 +0300

    LeftiestHeap error has been fixed

--- src/LeftiestHeap.h
+++ src/LeftiestHeap.h
@@ -11,5 +11,11 @@ namespace Heaps {
 		size_t _rank(Node* root);
 		virtual void _fixHeap(BinaryTreeBasedHeap::Node* givenRoot) override;
 		virtual TreeBasedHeap::Node* _makeNode(int key) override;
+		void _printHeap(Heaps::BinaryTreeBasedHeap::Node*root);
+		int _realRank(Node* root);
+#ifdef HEAP_TESTING
+		FRIEND_TEST(LeftiestTest, Structure);
+		FRIEND_TEST(LeftiestTest, Ranks);
+#endif // HEAP_TESTING
 	};
 };
